78e2d9e0ebf8eea0b18ebaab5004386b6c66a647
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2022-12-26 21:10:58 +1030

    fixed mysql and updated style

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'secret',
   cookie: {
     expires: 7200000
   },
